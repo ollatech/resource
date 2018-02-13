98f2add3744b232e896a4f215db65523bcd28aa1
Olla Tech <olla.code@gmail.com>
2018-02-13 15:38:42 +0700

    global update

--- src/Services/Collection/Query.php
+++ src/Services/Collection/Query.php
@@ -1,7 +1,7 @@
 <?php
 namespace Olla\Resource\Services\Collection;
 
-use Olla\Prisma\MetadataInterface;
+use Olla\Prisma\Metadata;
 use Olla\Flow\Repository;
 use Olla\Flow\Guard;
 use Olla\Flow\Validator;
@@ -9,20 +9,18 @@ use Olla\Resource\Collection;
 
 final class Query implements Collection
 {
-	protected $metatada;
+    protected $metatada;
     protected $repository;
     protected $guard;
     protected $validator;
-    public function __construct(MetadataInterface $metadata, Repository $repository, Guard $guard, Validator $validator) {
-    	$this->metadata = $metadata;
+    public function __construct(Metadata $metadata, Repository $repository, Guard $guard, Validator $validator) {
+        $this->metadata = $metadata;
         $this->repository = $repository;
         $this->guard = $guard;
         $this->validator = $validator;
     }
     public function get(string $resourceClass,  array $args = [], array $context = []) {
-
-        
-        
+        return $this->repository->get($resourceClass)->get($args);
     }
     private function validate(array $args = [], array $context = []) {
         
